20100366aaed20f6d8f3ef5fffab3726984efac7
FrozenFalcon-Byte <72398926+FrozenFalcon-Byte@users.noreply.github.com>
2024-05-27 11:11:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -49,15 +49,37 @@ ScrollReveal().reveal(".about_content .section_description", {
 });
 
 
-const swiper = new Swiper(".swiper", {
+var MagazineSlider = new Swiper('.swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
     loop: true,
-
-    pagination: {
-        el: ".swiper-pagination",
+    slidesPerView: 'auto',
+    coverFlowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
     },
-
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
+    }
 });
+
+// const swiper = new Swiper(".swiper", {
+//     loop: true,
+
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+// });
